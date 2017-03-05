d670329aae94f5ac40ece79de04c01e83fb27a86
Shadrach J <Shadrach J>
2017-03-05 20:39:19 +0700

    my repo

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('http://shadrach-hello.cfapps.io/').
+    $http.get('https://shadrach-hello.cfapps.io/').
         then(function(response) {
             $scope.hello = response.data;
             console.log($scope.hello.response);
